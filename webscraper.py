de39f06832119868f974e7fc30dcac8e99fbb724
Kevin Li <kevin.li.wa@gmail.com>
2019-04-12 19:15:02 -0700

    Full metacritic scrap of games

--- webscraper.py
+++ webscraper.py
@@ -9,43 +9,56 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 
-df = pd
+df = pd.DataFrame(columns=["Title", "Meta_Score", "User_score", "Release_Date", "Console"])    
+
 
 page = 1
 url = 'https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?view=condensed&page={page}'
 
 with requests.Session() as session:
-       while True:
+       while page < 163:
               req = Request(url, headers ={'User-Agent':' Mozilla/5.0'})
               webpage = urlopen(req).read()
 
               page_soup = soup(webpage, "html.parser")
-              page_soup
               main_container = page_soup.findAll("div", "col main_col")
 
               containers = main_container[0].findAll("div", "product_wrap")
               for container in containers:
-                     #print (container)
-                     games_string = container.find("a", href = True)['href']
-                     print (games_string)
+                     games_string = container.find("a", href = True)['href'].split("/")
+                     game = games_string[3].replace("-"," ")
+                     console = games_string[2].replace("-"," ")
+
                      meta_score = container.find("div", "metascore_w small game positive").contents[0]
-                     print (meta_score)
                      user_score = container.find('span', class_ = "data").contents[0]
-                     print (user_score)
+
                      rawdate = container.find('li', 'stat release_date full_release_date')
                      date = rawdate.find('span', 'data').contents[0]
-                     print (date)
-              #print (container.findAll("div", "data textscore textscore_favorable"))
-                     #print (container.findAll("span", "data"))
+              
+                     game_data = {'Title':[game], "Meta_Score":[meta_score], "User_score":[user_score], "Release_Date": [date], "Console":[console]}
+                     df = df.append(pd.DataFrame(data = game_data))
 
               page += 1
 
 
-games_string['href']
-containers[0].findAll("a")['href']
+req = Request(url, headers ={'User-Agent':' Mozilla/5.0'})
+webpage = urlopen(req).read()
 
-#sessions = requests.Session()   
-#response = sessions.get(url, timeout = 5)
-#content = BeautifulSoup(response.content, headers={'User-Agent': 'Mozilla/5.0'})
+page_soup = soup(webpage, "html.parser")
+page_soup
+main_container = page_soup.findAll("div", "col main_col")
 
-#print (content)
+containers = main_container[0].findAll("div", "product_wrap")
+for container in containers:
+       games_string = container.find("a", href = True)['href'].split("/")
+       game = games_string[3].replace("-"," ")
+       console = games_string[2].replace("-"," ")
+
+       meta_score = container.find("div", "metascore_w small game positive").contents[0]
+       user_score = container.find('span', class_ = "data").contents[0]
+
+       rawdate = container.find('li', 'stat release_date full_release_date')
+       date = rawdate.find('span', 'data').contents[0]
+     
+       game_data = {'Title':[game], "Meta_Score":[meta_score], "User_score":[user_score], "Release_Date": [date], "Console":[console]}
+       df = df.append(pd.DataFrame(data = game_data))

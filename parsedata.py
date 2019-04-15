38243020c93f1f94fb5d4498efa6a008d74d88fa
Kevin Li <kevin.li.wa@gmail.com>
2019-04-14 23:01:15 -0700

    VG data scrapper started

---
+++ parsedata.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import Request, urlopen
+import requests
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+import numpy as np
+import pandas as pd
+import re
+
+df = pd.DataFrame(columns=["title", "console", "publisher", "developer", "vg_Score","meta_Score"
+                           "total_Ship", "total_Sale","na_Sale", "pal_Sale", "japan_Sale", "other_sale"
+                           "relase_Date", "last_Update"])    
+
+page = 1
+url1 = 'http://www.vgchartz.com/games/games.php?page=1'
+url2 = '&results=10&name=&console=&keyword=&publisher=&genre=&order=Sales&ownership=Both'
+url3 = '&boxart=Both&banner=Both&showdeleted=&region=All&goty_year=&developer=&direction=DESC'
+url4 = '&showtotalsales=1&shownasales=1&showpalsales=1&showjapansales=1&showothersales=1&showpublisher=1'
+url5 = '&showdeveloper=1&showreleasedate=1&showlastupdate=1&showvgchartzscore=1&showcriticscore=1&showuserscore=1'
+url6 = '&showshipped=1&alphasort=&showmultiplat=No'
+
+url = url1 + url2 + url3 +url4 + url5 + url6
+
+
+with requests.Session() as session:
+       while page < 1:
+              req = Request(url, headers ={'User-Agent':' Mozilla/5.0'})
+              webpage = urlopen(req).read()
+
+              page_soup = soup(webpage, "html.parser")
+              main_container = page_soup.find("div", id = "generalBody")
+
+
+
+              containers = main_container[0].findAll("div", "product_wrap")
+              for container in containers:
+                     games_string = container.find("a", href = True)['href']
+                     game = games_string.split("/")[3].replace("-"," ")
+                     console = games_string.split("/")[2].replace("-"," ")
+                     game_url = 'https://www.metacritic.com' + str(games_string)
+
+                     meta_score = container.find("div", "metascore_w small game positive").contents[0]
+                     user_score = container.find('span', style = True).contents[0]
+
+                     rawdate = container.find('li', 'stat release_date full_release_date')
+                     date = rawdate.find('span', 'data').contents[0]
+                     
+                     game_data = {'title':[game], "meta_Score":[meta_score], "user_Score":[user_score], "release_Date": [date], "console":[console],
+                                  "url":[game_url]}
+                     df = df.append(pd.DataFrame(data = game_data)).reset_index(drop = True)
+
+              page += 1
+
+
+def test(req):
+    req = Request(url, headers ={'User-Agent':' Mozilla/5.0'})
+    webpage = urlopen(req).read()
+
+    return soup(webpage, "html.parser")                
--- webscraper.py
+++ webscraper.py
@@ -9,13 +9,15 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame(columns=["Title", "Meta_Score", "User_score", "Release_Date", "Console", "url"])    
+df = pd.DataFrame(columns=["title", "meta_Score", "user_Score", "release_Date", "console", "url",
+                           "user_Pos", "user_Mix", "user_Neg", 'crit_Pos','crit_Mix','crit_Neg','pos_Review',
+                           'mix_Review', 'neg_Review', 'prod_Rating'])    
 
 page = 1
 url = 'https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?view=condensed&page={page}'
 
 with requests.Session() as session:
-       while page < 2:
+       while page < 164:
               req = Request(url, headers ={'User-Agent':' Mozilla/5.0'})
               webpage = urlopen(req).read()
 
@@ -27,7 +29,7 @@ with requests.Session() as session:
                      games_string = container.find("a", href = True)['href']
                      game = games_string.split("/")[3].replace("-"," ")
                      console = games_string.split("/")[2].replace("-"," ")
-                     game_url = 'https://www.metacritic.com' + str(games_string) + '/user-reviews'
+                     game_url = 'https://www.metacritic.com' + str(games_string)
 
                      meta_score = container.find("div", "metascore_w small game positive").contents[0]
                      user_score = container.find('span', class_ = "data").contents[0]
@@ -35,20 +37,42 @@ with requests.Session() as session:
                      rawdate = container.find('li', 'stat release_date full_release_date')
                      date = rawdate.find('span', 'data').contents[0]
                      
-                     game_data = {'Title':[game], "Meta_Score":[meta_score], "User_score":[user_score], "Release_Date": [date], "Console":[console], "url":[game_url]}
+                     game_data = {'title':[game], "meta_Score":[meta_score], "user_Score":[user_score], "release_Date": [date], "console":[console],
+                                  "url":[game_url]}
                      df = df.append(pd.DataFrame(data = game_data)).reset_index(drop = True)
 
               page += 1
 
 with requests.Session() as session:
-       for game in df['url']:
-              req = Request(game, headers ={'User-Agent':' Mozilla/5.0'})
+       for url in df['url']:
+              req = Request(url, headers ={'User-Agent':' Mozilla/5.0'})
               webpage = urlopen(req).read()
 
               page_soup = soup(webpage, "html.parser")
               container = page_soup.findAll("li", "score_count")
-              pos_score = container[0].find("span", "count").contents[0]
-              mix_score = container[1].find("span", "count").contents[0]
-              neg_score = container[2].find("span", "count").contents[0]
+              user_container = page_soup.findAll("ol", "score_counts hover_none")
+              crit_container = page_soup.find_all("ol", "score_counts")
+              sub_container = container[0].findAll("li", "score_count")
+              sub_container[0]
 
+              df['user_Pos'][df['url'] == url] = container[0].find("span", "count").contents[0]
+              df['user_Mix'][df['url'] == url] = container[1].find("span", "count").contents[0]
+              df['user_Neg'][df['url'] == url] = container[2].find("span", "count").contents[0]
+              df['crit_Pos'][df['url'] == url] = container[3].find("span", "count").contents[0]
+              df['crit_Mix'][df['url'] == url] = container[4].find("span", "count").contents[0]
+              df['crit_Neg'][df['url'] == url] = container[5].find("span", "count").contents[0]
+              
+with requests.Session() as session:
+       for url in df['url']:
+              req = Request(url+ '/user-reviews', headers ={'User-Agent':' Mozilla/5.0'})
+              webpage = urlopen(req).read()
 
+              page_soup = soup(webpage, "html.parser")
+              container = page_soup.find("div", "module reviews_module user_reviews_module")
+              
+              df['pos_Review'][df['url'] == game] = container.findAll("span", "count")[0].contents[0]
+              df['mix_Review'][df['url'] == game] = container.findAll("span", "count")[1].contents[0]
+              df['neg_Review'][df['url'] == game] = container.findAll("span", "count")[2].contents[0]
+
+              prod_container = page_soup.find('li', 'summary_detail product_rating')
+              df['prod_Rating'][df['url'] == game]  = prod_container.find('span', 'data').contents[0]
